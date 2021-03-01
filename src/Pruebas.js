6784fa12b0b30eb4373697e43e4578e2421744bf
Dani <danicisneros2@hotmail.com>
2021-02-28 22:26:49 -0500

    Datepicker y check en form

--- src/Pruebas.js
+++ src/Pruebas.js
@@ -1,24 +1,47 @@
 import React ,{useState}from "react";
-import {Checkbox} from "antd";
-import Form from "antd/lib/form/Form";
-import FormItem from "antd/lib/form/FormItem";
-
+import { Form, Checkbox, DatePicker,Button } from 'antd';
 
 const Pruebas = () => {
 
+    const [form] = Form.useForm();
     const[checked,setChecked]=useState(false);
     const onChange = (e) => {
-        console.log('checked = ', e.target.checked);
+        if(e.target.checked){
+            form.setFieldsValue({
+                prueba1:1,
+            });
+        }else{
+            form.setFieldsValue({
+                prueba1:0,
+            });
+        }
         setChecked(!checked);
     };
+    const onFinish =(fieldsValue)=>{
+        const values = {
+          ...fieldsValue,
+          'fecha': fieldsValue['fecha'].format('YYYY-MM-DD'),
+        };
+        console.log('Received values of form: ', values);
+    }
 
     const label = `${checked? 'Checked' : 'Unchecked'}`;
     return(
         <>
-          <Form > 
-          <FormItem label={label}>
-            <Checkbox onChange={onChange}/>
-        </FormItem>
+        <Form 
+        form={form}
+        onFinish={onFinish}> 
+            <Form.Item name="prueba1" label={label}>
+                <Checkbox onChange={onChange}/>
+            </Form.Item>
+            <Form.Item name="fecha">
+                <DatePicker/>
+            </Form.Item>
+            <Form.Item >
+                <Button type="primary" htmlType="submit">
+                Enviar
+                </Button>
+            </Form.Item>
         </Form> 
         </>
     ); 
